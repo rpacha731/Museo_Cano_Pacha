5fb41b81a0d05b39bfdd70f7bc7e8d4394a82f8b
Leonel Pacha <rpacha731@alumnos.iua.edu.ar>
2021-10-10 20:43:53 -0300

    Login

---
+++ app/src/main/java/com/tecno_moviles/museo_cano_pacha/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.tecno_moviles.museo_cano_pacha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tecno_moviles.museo_cano_pacha.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
