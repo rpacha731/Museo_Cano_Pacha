4f2880c09380432dc5af2c3ce9f0b3032b22c003
Leonel Pacha <rpacha731@alumnos.iua.edu.ar>
2021-10-12 22:28:19 -0300

    no se subio esto xd

--- app/src/main/java/com/tecno_moviles/museo_cano_pacha/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tecno_moviles/museo_cano_pacha/ui/home/HomeFragment.kt
@@ -13,8 +13,6 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +26,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = binding.textHome
-        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
-        //    textView.text = it
-        //})
         return root
     }
 
