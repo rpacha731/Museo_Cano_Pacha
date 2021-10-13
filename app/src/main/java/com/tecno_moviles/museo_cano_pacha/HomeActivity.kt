faaa7e7dbe177973970f74ab463da8b25d51a884
Leonel Pacha <rpacha731@alumnos.iua.edu.ar>
2021-10-12 22:27:52 -0300

    Home corregido

--- app/src/main/java/com/tecno_moviles/museo_cano_pacha/HomeActivity.kt
+++ app/src/main/java/com/tecno_moviles/museo_cano_pacha/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.tecno_moviles.museo_cano_pacha
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,36 +10,39 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.tecno_moviles.museo_cano_pacha.databinding.ActivityMainBinding
+import com.tecno_moviles.museo_cano_pacha.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setSupportActionBar(binding.appBarMain.toolbar)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_perfil, R.id.nav_conf, R.id.nav_ayuda, R.id.nav_cerrar_sesion
+                R.id.nav_home,
+                R.id.nav_perfil,
+                R.id.nav_conf,
+                R.id.nav_ayuda,
+                R.id.nav_cerrar_sesion
             ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
--- app/src/main/java/com/tecno_moviles/museo_cano_pacha/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/tecno_moviles/museo_cano_pacha/ui/perfil/PerfilFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tecno_moviles.museo_cano_pacha.databinding.FragmentPerfilBinding
 
@@ -15,8 +13,6 @@ class PerfilFragment : Fragment() {
     private lateinit var perfilViewModel: PerfilViewModel
     private var _binding: FragmentPerfilBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,10 +26,6 @@ class PerfilFragment : Fragment() {
         _binding = FragmentPerfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = binding.textGallery
-        //ayudaViewModel.text.observe(viewLifecycleOwner, Observer {
-        //    textView.text = it
-        //})
         return root
     }
 
