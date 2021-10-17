78789b7f054379451d038be5f0d693e9e9a2301f
ecano043 <ecano043@alumnos.iua.edu.ar>
2021-10-16 23:18:56 -0300

    ResultActivity

---
+++ app/src/main/java/com/tecno_moviles/museo_cano_pacha/resultado_qr/ResultadoActivity.kt
@@ -0,0 +1,12 @@
+package com.tecno_moviles.museo_cano_pacha.resultado_qr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tecno_moviles.museo_cano_pacha.R
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
